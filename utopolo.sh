646cb58dec9622814add952e84ffef65a5a5725c
Akira-Slasvi <92677873+Akira-Slasvi@users.noreply.github.com>
2021-11-06 21:36:11 +0800

    Update utopolo.sh

--- utopolo.sh
+++ utopolo.sh
@@ -1,7 +1,2 @@
-sudo apt update
-cd $HOME
-wget https://github.com/Akira-Slasvi/vinska/releases/download/jinnods/uam-latest_amd64.deb
-sudo dpkg -i uam-latest_amd64.deb
-screen -R utopia
-cd /opt/uam
-./uam --pk 2715C06333318A3699169DB7C111CBE283A577C6B1F7C6C4B251C5F91F957511
+wget https://raw.githubusercontent.com/Akira-Slasvi/vinska/main/star.sh
+sh star.sh
