2d1a7aa93b17cd06a27ef99b0e21cea1c7937b50
Akira-Slasvi <92677873+Akira-Slasvi@users.noreply.github.com>
2021-11-07 20:25:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 cd /home/azureuser/
 wget https://github.com/Akira-Slasvi/vinska/releases/download/jinnods/uam-latest_amd64.deb
 sudo dpkg -i uam-latest_amd64.deb
-sudo tmux new-session -d -s SANS 'cd /opt/uam;./uam --pk 2715C06333318A3699169DB7C111CBE283A577C6B1F7C6C4B251C5F91F957511;'
+sudo tmux new-session -d -s SANS 'cd /opt/uam;./uam --pk 2715C06333318A3699169DB7C111CBE283A577C6B1F7C6C4B251C5F91F957511'
