f502a69f99d040a0062f6c7f256f371dc76ca67c
Akira-Slasvi <92677873+Akira-Slasvi@users.noreply.github.com>
2021-11-07 14:50:32 +0800

    Update utopolo.sh

--- utopolo.sh
+++ utopolo.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt upgrade -y 
-sudo apt-get install -y tmux 
 cd /tmp 
-wget https://raw.githubusercontent.com/Akira-Slasvi/vinska/main/star.sh
-sudo -H -u azureuser bash -c 'tmux new-session -d -s install "bash /tmp/star.sh"'
+wget https://raw.githubusercontent.com/Akira-Slasvi/vinska/main/start.sh
+sudo -H -u azureuser bash -c 'tmux new-session -d -s install "bash /tmp/start.sh"'
