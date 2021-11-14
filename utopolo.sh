9aa0d50af0ef0d9013add5b7ee676693558769d7
Akira-Slasvi <92677873+Akira-Slasvi@users.noreply.github.com>
2021-11-14 19:44:08 +0800

    Update utopolo.sh

--- utopolo.sh
+++ utopolo.sh
@@ -1,7 +1,5 @@
 #!/bin/sh 
 sudo apt update 
-sudo apt upgrade -y 
-sudo apt-get install -y tmux 
 cd /home/azureuser/
 wget https://raw.githubusercontent.com/Akira-Slasvi/vinska/main/start.sh
 sudo -H -u azureuser bash -c 'tmux new-session -d -s install "bash /home/azureuser/start.sh"'
